b6e13a0b0f640e9f94333fb9f095c498a70a3b8e
Nigel Tan <nigel.tan@u.nus.edu>
2023-05-06 15:28:29 +0800

    check if jwt.eid === emp_id

--- auth/auth.js
+++ auth/auth.js
@@ -2,9 +2,6 @@ const Employees = require("../models/employeeModel");
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 
-// @desc Login
-// @route POST /auth
-// @access Public
 const login = asyncHandler(async (req, res) => {
   const { emp_id, password } = req.body;
 
@@ -45,6 +42,17 @@ const login = asyncHandler(async (req, res) => {
   res.json({ accessToken });
 });
 
+const verify_credentials = (accessToken, emp_id) => {
+  try {
+    const decoded = jwt.verify(accessToken, "super_secret_access_key");
+    return decoded.employeeId === emp_id;
+  }
+  catch (error) {
+    return false;
+  }
+};
+
+/*
 // @desc Refresh
 // @route GET /auth/refresh
 // @access Public - because access token has expired
@@ -98,9 +106,6 @@ const logout = (req, res) => {
   res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
   res.json({ message: "Cookie cleared" });
 };
+*/
 
-module.exports = {
-  login,
-  refresh,
-  logout,
-};
+module.exports = { login, verify_credentials };
